9b0d13af6a65ef9bff14d0567cb1745f31a500a0
Kai Hergenroether <kai@hergenrother.de>
2015-03-16 01:05:46 +0100

    Began restoring rpi compilation.

--- prj/Raspiator/src/JenkinsAccess.h
+++ prj/Raspiator/src/JenkinsAccess.h
@@ -5,6 +5,7 @@
 #include <string>
 #include <queue>
 #include <mutex>
+#include <condition_variable>
 #include <thread>
 #include <random>
 
--- prj/Raspiator/src/main.cpp
+++ prj/Raspiator/src/main.cpp
@@ -1,14 +1,8 @@
 #include <iostream>
 #include <GLES/gl.h>
-#include <Vectoid/PerspectiveProjection.h>
-#include <Vectoid/Camera.h>
-#include <Vectoid/CoordSys.h>
-#include <Vectoid/Geode.h>
-
-#include "Glyphs.h"
-#include "Indicatower.h"
+#include <Vectoid/Vector.h>
 #include "VideoCoreDisplay.h"
-#include "TextConsole.h"
+#include "Visualization.h"
 
 using namespace std;
 using namespace kxm::Vectoid;
@@ -16,28 +10,42 @@ using namespace Raspiator;
 
 
 int main(int argc, char **argv) {
+    puts("Buh!");
     cout << "rpi OpenGL ES test" << endl;
+    exit(0);
+    
+    Visualization vis("jenkins.org", 1.0f, .4f, .0015f, .08f, .11f, -2.0f,
+                      Vector(-1.0f, 0.0f, 0.0f));
+    vis.NewGroup("Pre-Production Build (PPB)", 0.0f, 0.0f);
+    vis.NewSubGroup("Builds", 0.0f);
+    vis.NewJob("ppb_build",         "MPC Trunk",                "a", 120.0f);
+    vis.NewJob("ppb_build_sim",     "MPC Simulation Trunk",     "b", 120.0f);
+    vis.NewJob("ppb_build_kw",      "MPC Klocwork Trunk",       "c", 120.0f);
+    vis.NewJob("ppb_build_cov",     "MPC Coverage Trunk",       "d", 120.0f);
+    vis.NewJob("ppb_build_cov_int", "MPC Coverage Integration", "e", 120.0f);
+    vis.NewSubGroup("Module Tests", 0.0f);
+    vis.NewJob("ppb_mod_smoke",     "Tests Trunk Smoke",        "f", 120.0f);
+    vis.NewJob("ppb_mod_normal",    "Tests Trunk Normal",       "g", 120.0f);
+    vis.NewJob("ppb_mod_intensive", "Tests Trunk Intensive",    "h", 120.0f);
+    vis.NewSubGroup("System Tests", 0.0f);
+    vis.NewJob("ppb_sys_smoke",     "System Smoke Trunk",       "i", 120.0f);
+    vis.NewJob("ppb_sys_7",         "System Function Trunk",    "j", 120.0f);
+    vis.NewJob("ppb_sys_8",         "System Lasttest Trunk",    "k", 120.0f);
+    vis.NewJob("ppb_sys_9",         "System Calibration Trunk", "l", 120.0f);
+    vis.NewJob("ppb_sys_10",        "System Regression Trunk",  "m", 120.0f);
+    vis.NewGroup("Entwicklungsmuster (EM)", 80.0f, 0.0f);
+    vis.NewSubGroup("Builds", 105.0f);
+    vis.NewJob("em_build",          "MPC Branch",               "n", 120.0f);
+    vis.NewJob("em_build_sim",      "MPC Simulation Branch",    "o", 120.0f);
+    vis.NewSubGroup("Module Tests", 0.0f);
+    vis.NewJob("em_mod_smoke",      "Tests Branch Smoke",       "p", 120.0f);
+    vis.NewJob("em_mod_normal",     "Tests Branch Normal",      "q", 120.0f);
+    vis.NewJob("em_mod_intensive",  "Tests Branch Intensive",   "r", 120.0f);
+    vis.UpdateLayout();
     
     VideoCoreDisplay display;
+    vis.SetViewPort(display.Width(), display.Height());
     cout << "display_size=(" << display.Width() << ", " << display.Height() << ")" << endl;
-    
-    auto projection = make_shared<PerspectiveProjection>();
-    auto camera     = make_shared<Camera>();
-    auto coordSys   = make_shared<CoordSys>();
-    auto tower      = make_shared<Indicatower>();
-    auto glyphs     = make_shared<Glyphs>();
-    auto console    = make_shared<TextConsole>(10, 2, glyphs);
-    auto geode      = make_shared<Geode>(console);
-    //auto geode      = make_shared<Geode>(tower);
-    projection->AddChild(camera);
-    camera->AddChild(coordSys);
-    coordSys->AddChild(geode);
-    
-    projection->SetViewPort((float)display.Width(), (float)display.Height());
-    projection->SetWindowSize(1.0f);
-    projection->SetViewingDepth(4.0f);
-    projection->SetEyepointDistance(1.0f);
-    camera->SetPosition(Vector(0.0f, 0.5f, 2.5f));
     
     display.BeginFrame();
     glEnable(GL_DEPTH_TEST);
@@ -50,16 +58,11 @@ int main(int argc, char **argv) {
     glEnable(GL_CULL_FACE);
     display.EndFrame();
     
-    for (int i = 0; i < 2; ++i) {
-        for (int angle = 0; angle < 360; angle += 1) {
-            Transform transform(ZAxis, (float)angle);
-            coordSys->SetTransform(transform);
-            
-            display.BeginFrame();
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-            projection->Render(nullptr);
-            display.EndFrame();
-        }
+    for (int i = 0; i < 300; ++i) {           
+        display.BeginFrame();
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        vis.RenderFrame();
+        display.EndFrame();
     }
     
     return 0;
