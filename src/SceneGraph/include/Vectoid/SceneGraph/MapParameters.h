0325f31560f31c1197fb7b1d1bdf5aaab1b67a5d
Kai Hergenröther <kai@hergenrother.de>
2026-02-05 17:44:16 +0100

    Fixed compilation for App.

--- src/SceneGraph/include/Vectoid/SceneGraph/MapParameters.h
+++ src/SceneGraph/include/Vectoid/SceneGraph/MapParameters.h
@@ -17,42 +17,34 @@ namespace SceneGraph {
 
 //! Centrally holds parameters for a given map ("world", "level", ...).
 /*!
- *  \param numCellsX
- *  \param numCellsZ
- *  Each must be even and at least <c>2</c>.
- *  
- *  \param cellSize
- *  A cell's projection into the <c>xz</c>-plane is a square (see above). This parameter controls
- *  the square's edge length.
- *
- *  Depending on the aforementioned parameters, the resulting terrain will cover in the
- *  <c>xz</c>-plane the axis-parallel, rectangular area given by <c>(x = -numCellsX/2 * cellSize,
- *  z = -numCellsZ/2 * cellSize)</c> and <c>(x = numCellsX/2 * cellSize, z = numCellsZ/2
- *  * cellSize)</c>.
- *
- *  \param numVisibleCellsX
- *  \param numVisibleCellsZ
- *  Number of cells in <c>x</c> and <c>z</c> directions, respectively, that are visible at a given
- *  point in time. Must both be even and at least <c>2</c>, and less or equal to their respective
- *  <c>numCells</c> parameter. Which subset of the terrain's cells is actually visible is controlled
- *  via the observer position set via SetObserverPosition().
+ *  The resulting terrain will cover in the <c>xz</c>-plane the axis-parallel, rectangular area given by
+ *  <c>(x = -numCellsX/2 * cellSize, z = -numCellsZ/2 * cellSize)</c> and
+ *  <c>(x = numCellsX/2 * cellSize, z = numCellsZ/2 * cellSize)</c>.
  */
 struct MapParameters {
-    int                         numCellsX, numCellsZ,
-                                numVisibleCellsX, numVisibleCellsZ;
-    float                       cellSize,
-                                gravity,
-                                landerThrust,
-                                cameraMinHeight,
-                                maxThrusterParticleAge,
-                                maxShotParticleAge,
-                                thrusterExhaustVelocity,
-                                thrusterExhaustInterval,
-                                thrusterParticleSpread,
-                                thrusterJetSize,
-                                shotVelocity,
-                                shotFiringInterval;
-    Vectoid::Core::Range<float> xRange, zRange;
+    int                         numCellsX;            //! Must be even and at least <c>2</c>.
+    int                         numCellsZ;            //! Must be even and at least <c>2</c>.
+    int                         numVisibleCellsX;     //! Number of cells in <c>x</c> and <c>z</c> directions,
+    int                         numVisibleCellsZ;     //! respectively, that are visible at a given point in time. Must
+                                                      //! both be even and at least <c>2</c>, and less or equal to
+                                                      //! their respective <c>numCells</c> parameter. Which subset of
+                                                      //! the terrain's cells is actually visible is controlled via the
+                                                      //! observer position set via SetObserverPosition().
+    float                       cellSize;             //! A cell's projection into the <c>xz</c>-plane is a square (see
+                                                      //! above). This parameter controls the square's edge length.
+    float                       gravity;
+    float                       landerThrust;
+    float                       cameraMinHeight;
+    float                       maxThrusterParticleAge;
+    float                       maxShotParticleAge;
+    float                       thrusterExhaustVelocity;
+    float                       thrusterExhaustInterval;
+    float                       thrusterParticleSpread;
+    float                       thrusterJetSize;
+    float                       shotVelocity;
+    float                       shotFiringInterval;
+    Vectoid::Core::Range<float> xRange;
+    Vectoid::Core::Range<float> zRange;
     int                         numStars;
     Vectoid::Core::Range<float> starFieldCoordRange;
     float                       starFieldMinHeight;
